a2d6c944d125ecde2bea8179dc58b0e8a701c6bc
ejejeokon <52031275+ejejeokon@users.noreply.github.com>
2019-11-28 07:47:35 -0800

    Chaining time

--- myApp.js
+++ myApp.js
@@ -31,7 +31,18 @@ var app = express();
 
 
 /** 8) Chaining middleware. A Time server */
-
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.send({
+      time: req.time
+    });
+  }
+);
 
 /** 9)  Get input from client - Route parameters */
 
